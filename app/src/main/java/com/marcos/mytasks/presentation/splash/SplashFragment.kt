5149bf1c9d77931d90058e534c1f712fb41873f1
Marcos <mramosmarcos14@gmail.com>
2023-02-25 13:23:41 -0300

    Configure splash navigation for login screen

--- app/src/main/java/com/marcos/mytasks/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/marcos/mytasks/presentation/splash/SplashFragment.kt
@@ -1,20 +1,42 @@
 package com.marcos.mytasks.presentation.splash
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.marcos.mytasks.R
+import com.marcos.mytasks.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
+    private lateinit var binding: FragmentSplashBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    ) = FragmentSplashBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        binding = this
+    }.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth, TIME)
+    }
+
+    private fun checkAuth() {
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+    }
+
+    companion object {
+        private const val TIME = 3000L
     }
 
 }
