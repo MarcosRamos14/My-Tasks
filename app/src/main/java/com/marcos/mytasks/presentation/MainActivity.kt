4866945eee82750006dfc29d9fec117ee2b57d96
Marcos <mramosmarcos14@gmail.com>
2023-02-25 11:58:34 -0300

    Reference navigation graph in ActivityMain and instatiate NavController

--- app/src/main/java/com/marcos/mytasks/presentation/MainActivity.kt
+++ app/src/main/java/com/marcos/mytasks/presentation/MainActivity.kt
@@ -2,11 +2,24 @@ package com.marcos.mytasks.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
 import com.marcos.mytasks.R
+import com.marcos.mytasks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
     }
 }
