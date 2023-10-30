dcbd2340de1737c260cdf61b8f900385dc302faf
Marcos <mramosmarcos14@gmail.com>
2023-10-30 16:36:08 -0300

    chore: Temporarily hide Facebook login button.

--- app/src/main/java/com/marcos/mytasks/framework/di/NetworkModule.kt
+++ app/src/main/java/com/marcos/mytasks/framework/di/NetworkModule.kt
@@ -1,10 +1,15 @@
 package com.marcos.mytasks.framework.di
 
+import android.content.Context
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 import com.marcos.mytasks.BuildConfig
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
@@ -12,10 +17,21 @@ import dagger.hilt.components.SingletonComponent
 object NetworkModule {
 
     @Provides
+    fun providesFirebaseAuth() = FirebaseAuth.getInstance()
+
+    @Provides
     fun providesGoogleSignInOptions() : GoogleSignInOptions {
         return GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestEmail()
             .requestIdToken(BuildConfig.WEB_CLIENT_ID)
             .build()
+    }
+
+    @Provides
+    fun provideGoogleSignInClient(
+        @ApplicationContext context: Context,
+        googleSignInOptions: GoogleSignInOptions
+    ): GoogleSignInClient {
+        return GoogleSignIn.getClient(context, googleSignInOptions)
     }
 }
